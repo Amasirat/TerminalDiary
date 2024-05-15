a7437c23a19cda9efed05da3446a701f4fdf24f8
amasirat <amir.basirat.2004@gmail.com>
2024-05-15 15:03:25 +0330

    added comments

--- Lib/Diary.cs
+++ Lib/Diary.cs
@@ -41,12 +41,14 @@ public class Diary
     }
 
     public string Name {get; set;}
+    // events to communicate with outside classes
     public event UploadEventHandler ReadyToUpload;
     public event DisplayEventHandler ReadyToDisplay;
+
     public List<DiaryEntry> Entries 
     {
         get { return Entries; }
     }
-    private delegate void UploadEventHandler(Diary diary);
-    private delegate void DisplayEventHandler(Diary diary);
+    private delegate void UploadEventHandler(Diary diary);//Used to hook up a database uploader of some kind
+    private delegate void DisplayEventHandler(Diary diary);//Used to hook up to the console
 }
