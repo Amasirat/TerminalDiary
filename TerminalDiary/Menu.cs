2792a7f0e6591fba1fb5b2931857ad51e1afa5e1
amasirat <amir.basirat.2004@gmail.com>
2024-08-28 01:20:48 +0330

    included comments on Menu abstract class

--- TerminalDiary/Menu.cs
+++ TerminalDiary/Menu.cs
@@ -1,10 +1,18 @@
 namespace TerminalDiary;
-
+// Used for displaying UI on console or Terminal
 public abstract class Menu
 {
+    // Used for dictating what selections a particular child of selection should contain
     public abstract void PopulateSelection(Dictionary<int, string> select);
-    public abstract void DisplaySelections();
-
+    // Used to display selections on console
+    public virtual void DisplaySelections()
+    {
+        foreach (KeyValuePair<int, string> selection in selections)
+        {
+            Console.WriteLine($"{selection.Key}.{selection.Value}");
+        }
+    }
+    // Used to handle user selection and do appropriate action given choice
     public abstract void ChoiceHandler(int choice);
 
     protected Dictionary<int, string>? selections;
